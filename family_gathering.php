1306e5a1f63d483fe8fb73952d39520b7416625c
Imanul Aufa <imanulaufa@gmail.com>
2023-02-13 10:33:30 +0700

    Signed-off-by: Imanul Aufa <imanulaufa@gmail.com>
    "Adding Family Gathering Page"

---
+++ family_gathering.php
@@ -0,0 +1,426 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Aboreto&display=swap" rel="stylesheet">
+
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+
+    <!-- my css -->
+    <link rel="stylesheet" href="css/family_gathering.css">
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- jquery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counterup.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" integrity="sha512-UJfAaOlIRtdR+0P6C3KUoTDAxVTuy3lnSXLyLKlHYJlcSU8Juge/mjeaxDNMlw9LgeIotgz5FP8eUQPhX1q10A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+    <link rel="icon" href="img/logo no bg.png" type="image/icon type">
+    <title>Sumatera Ophthalmologist Meeting 15th</title>
+
+    <style>
+        body {
+            background-image: url('img/');
+            background-attachment: fixed;
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+    </style>
+</head>
+
+<body class="amber lighten-2">
+
+    <!-- navbar -->
+
+
+
+    <div class="navbar-fixed">
+        <nav class="amber lighten-3">
+            <div class="container">
+                <div class="nav-wrapper">
+                    <a href="index.html"><img class="brand-logo img-responsive" src="img/logo_som2.png"></a>
+                    <a href="#" data-target="mobile-nav" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                    <ul class="right hide-on-med-and-down">
+                        <li><a class="teal-text text-darken-2" href="schedule.html">Schedule</a></li>
+                        <li><a class="teal-text text-darken-2" href="#speakers">Speakers</a></li>
+                        <li><a class="dropdown-trigger teal-text text-darken-2" href="#" data-target="dropdown1">Events<i class="material-icons right">arrow_drop_down</i></a></li>
+                        <li><a class="teal-text text-darken-2" href="https://bit.ly/som15sumbar" target="_blank">Registration</a></li>
+                        <li><a class="teal-text text-darken-2" href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+
+
+    <ul id="dropdown1" class="dropdown-content">
+        <li><a class="black-text" href="family_gathering.html">Family Gathering</a></li>
+    </ul>
+
+
+    <!-- sidenav -->
+    <ul class="sidenav" id="mobile-nav">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="#speakers">Speakers</a></li>
+        <li><a class="dropdown-trigger" href="#" data-target="dropdown2">Events<i class="material-icons right">arrow_drop_down</i></a></li>
+        <li><a href="https://bit.ly/som15sumbar" target="_blank">Registration</a></li>
+
+
+        <li><a href="#contact">Contacts</a></li>
+    </ul>
+    <ul id="dropdown2" class="dropdown-content">
+        <li><a href="family_gathering.html">Family Gathering</a></li>
+    </ul>
+
+
+
+    <!-- slider -->
+
+    <div id="home" class="slider">
+        <ul class="slides">
+            <li>
+                <img src="assets/ilustrasi/10046.jpg">
+
+            </li>
+            <!-- <li>
+                <img src="assets/ilustrasi/kantor-walikota-bukittinggi.jpg">
+
+            </li> -->
+
+
+            <!-- <li>            
+            <img src="img/balcone2.jpeg">
+            <div class="caption right-align white-text">
+                    <h3>SUMATERA OPHTHALMOLOGIST MEETING 15th</h3>
+                </div>
+            
+        </li>
+        <li>            
+            <img src="img/balcone1.jpg">
+            <div class="caption left-align white-text">
+                    <h2>Come Join Us!</h2> 
+                    <h3>SUMATERA OPHTHALMOLOGIST MEETING 15th</h3>
+                </div>
+        </li> -->
+        </ul>
+    </div>
+
+
+    <section class="logosom">
+        <div class="container">
+            <div class="row">
+                <div class="center col l12 m12 s12">
+                    <img width="65%" src="img/Logo-02.png" alt="">
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+    <div class="container">
+        <div class="row">
+            <!-- Card for Aerobics -->
+            <div class="col s12 m8 offset-m2 custom-width">
+                <!-- Card for Aerobics -->
+                <div class="card medium">
+                    <div class="card-image">
+                        <img src="assets/ilustrasi/fitness-g4964bd41a_1920.jpg" alt="Aerobics">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Aerobics</span>
+                        <p>Join us for a morning workout with Aerobics! Start your day off right and get your body moving.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="row">
+
+
+            <!-- Card for Fun Games -->
+            <div class="col s12 m6 offset-m4 custom-width">
+                <div class="card medium">
+                    <div class="card-image">
+                        <img src="assets/ilustrasi/family-ge4aef3fdd_1920.jpg" alt="Fun Games">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Fun Games</span>
+                        <p>Get ready for some friendly competition with fun games between groups!</p>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+        <div class="row">
+
+            <!-- Card for Idol Performance -->
+            <div class="col s12 m6 offset-m2 custom-width">
+                <div class="card medium">
+                    <div class="card-image">
+                        <img src="assets/ilustrasi/guitar-756326_1920.jpg" alt="Idol Performance">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Idol Performance</span>
+                        <p>Don't miss out on an exciting idol performance! See some of your favorite artists in action.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="row">
+
+            <!-- Card for Live Music -->
+            <div class="col s12 m6 offset-m4 custom-width">
+                <div class="card medium">
+                    <div class="card-image">
+                        <img src="assets/ilustrasi/microphone-2130806_1920.jpg" alt="Live Music">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Live Music</span>
+                        <p>Enjoy some live music and get your groove on! Sit back, relax, and enjoy the tunes.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="row">
+
+            <!-- Card for Food Stands -->
+            <div class="col s12 m6 offset-m2 custom-width">
+                <div class="card medium">
+                    <div class="card-image">
+                        <img src="assets/ilustrasi/supermarket-949913_1920.jpg" alt="Food Stands">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Food Stands</span>
+                        <p>Satisfy your hunger with delicious food from our food stands! There's something for everyone to enjoy.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+
+            <!-- Card for Food Stands -->
+            <div class="col s12 m6 offset-m4 custom-width">
+                <div class="card medium">
+                    <div class="card-image">
+                        <img src="assets/ilustrasi/sport-938797_1920.jpg" alt="Cycling Activity">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Cycling Adventure</span>
+                        <p>Get your heart pumping with a scenic cycling tour. Join us for an invigorating ride through the beautiful countryside and experience the thrill of exploring new terrain. Whether you're a seasoned cyclist or a beginner, there's a route for everyone. Don't miss out on this opportunity to get active and experience the great outdoors!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+
+    <!-- map location -->
+
+
+
+
+
+    <!-- <div class="location">
+            <div class="map-wrapper">
+            <div class="row">
+                <div class="col m4 s12">
+                    <div class="card-panel blue accent-4 center white-text">
+                        <i class="medium material-icons">location_on</i>
+                        <h5>Location</h5>                        
+                    </div>
+                    <ul class="collection with-header">
+                        <li class="collection-header"><h4>Our Office</h4></li>
+                        <li class="collection-item">PERDAMI SUMBAR</li>
+                        <li class="collection-item">Jl. Perumahan Pondok Indah Blok B/4 Jati Gaung, Padang</li>
+                        <li class="collection-item">West Sumatera, Indonesia</li>
+                    </ul>
+                </div>
+                <div class="col m8 s12">
+                
+                <iframe class="googlemap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15959.075213477023!2d100.3513275078125!3d-0.2854556000000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2fd54741546f9797%3A0x2088bb6f4eeaf55!2sKantor%20Walikota%20Bukittinggi!5e0!3m2!1sid!2sid!4v1675931739647!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                
+                </div>                
+        </div>
+        </div>
+    </div> -->
+
+    <section id="contact" class="contact grey lighten-3 scrollspy">
+        <div class="container">
+            <h3 class="bold blue-text text-accent-4 center">Contact Us</h3>
+            <div class="row">
+                <div class="col m5 s12">
+                    <div class="card-panel blue accent-4 center white-text">
+                        <i class="material-icons">email</i>
+                        <h5>Contact</h5>
+                        <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus sed id inventore rem aspernatur magni perspiciatis provident nihil amet sequi.</p> -->
+                    </div>
+                    <ul class="collection with-header">
+                        <li class="collection-header">
+                            <h4>Our Office</h4>
+                        </li>
+                        <li class="collection-item">PERDAMI SUMBAR</li>
+                        <li class="collection-item">Jl. Perumahan Pondok Indah Blok B/4 Jati Gaung, Padang</li>
+                        <li class="collection-item">West Sumatera, Indonesia</li>
+                    </ul>
+                </div>
+
+                <div class="col m7 s12">
+                    <form>
+                        <div class="card-panel">
+                            <h5>Please fill out this form</h5>
+
+                            <div class="input-field">
+                                <input type="text" name="name" id="name" required class="validate">
+                                <label for="name">Name</label>
+                            </div>
+                            <div class="input-field">
+                                <input type="email" name="email" id="email" required class="validate">
+                                <label for="email">Email</label>
+                            </div>
+                            <div class="input-field">
+                                <input type="text" name="phone" id="phone" required class="validate">
+                                <label for="phone">Phone Number</label>
+                            </div>
+                            <div class="input-field">
+                                <textarea name="message" id="message" class="materialize-textarea validate" required></textarea>
+                                <label for="message">Message</label>
+                            </div>
+
+                            <button type="submit" class="btn blue accent-4">SEND</button>
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <footer class="page-footer grey darken-3">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                    <h5 class="white-text">Contact</h5>
+                    <p class="grey-text text-lighten-4">+62 813 7845 5402</p>
+
+                </div>
+                <div class="col l4 offset-l2 s12">
+                    <h5 class="white-text">Connect</h5>
+                    <div class="socials">
+                        <ul>
+                            <li><a class="white-text" href="https://www.facebook.com/perdamisumbar2020" target="_blank"> <i class="fab fa-facebook-f"></i> Facebook</a></li>
+                            <li><a class="white-text" href="https://www.instagram.com/perdami.sumbar1/" target="_blank"><i class="fab fa-instagram"></i> Instagram</a></li>
+                            <li><a class="white-text" href="mailto:perdamisumbar2023@gmail.com"> <i class="fas fa-envelope" target="_blank"></i> Email</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="footer-copyright grey darken-4">
+            <div class="container center-align">
+                &COPY; 2023 PERDAMI SUMBAR | <a class="white-text" href="mailto:perdamisumbar2023@gmail.com"> perdamisumbar2023@gmail.com</a>
+            </div>
+        </div>
+    </footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!--JavaScript at end of body for optimized loading-->
+
+
+    </script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+
+
+
+    <script>
+        const sideNav = document.querySelectorAll('.sidenav');
+        M.Sidenav.init(sideNav);
+
+        const slider = document.querySelectorAll('.slider');
+        M.Slider.init(slider, {
+            indicators: false,
+            height: 500,
+            transition: 80,
+            interval: 4000
+        });
+
+
+
+
+
+
+
+        // $(document).ready(function(){
+        // $('.carousel').carousel();
+        // });
+
+        document.addEventListener('DOMContentLoaded', function() {
+            var elems = document.querySelectorAll('.carousel');
+            var instances = M.Carousel.init(elems, {
+
+                indicators: true,
+                height: 450
+
+            });
+        });
+
+
+
+        const parallax = document.querySelectorAll('.parallax');
+        M.Parallax.init(parallax);
+
+        const materialbox = document.querySelectorAll('.materialboxed');
+        M.Materialbox.init(materialbox);
+
+        const scroll = document.querySelectorAll('.scrollspy');
+        M.ScrollSpy.init(scroll);
+
+        const dropdown = document.querySelectorAll('.dropdown-trigger');
+        M.Dropdown.init(dropdown, {
+            coverTrigger: false,
+            constrainWidth: true
+        });
+    </script>
+
+
+
+
+
+</body>
+
+</html>
